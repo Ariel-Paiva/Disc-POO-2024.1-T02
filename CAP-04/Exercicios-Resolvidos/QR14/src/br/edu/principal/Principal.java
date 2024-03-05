216974fb4555d977fcd69077d41b2b8b9168208b
Ismael Nascimento <72841488+ismaelnascimento@users.noreply.github.com>
2024-03-05 14:41:39 -0300

    upload QR14

---
+++ CAP-04/Exercicios-Resolvidos/QR14/src/br/edu/principal/Principal.java
@@ -0,0 +1,32 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double sal, novo_sal, boni, aux;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o salário do funcionário: ");
+		sal = sc.nextDouble();
+		
+		if (sal <= 500) {
+			boni = sal * 5/100;
+		} else if (sal <= 1200) {
+			boni = sal * 12/100;
+		} else {
+			boni = 0;
+		}
+		
+		if (sal <= 600) {
+			aux = 150;
+		} else {
+			aux = 100;
+		}
+		
+		novo_sal = sal + boni + aux;
+		System.out.println("Salário acrescido de bonificação e auxílio escola: " + novo_sal);
+	}
+
+}
