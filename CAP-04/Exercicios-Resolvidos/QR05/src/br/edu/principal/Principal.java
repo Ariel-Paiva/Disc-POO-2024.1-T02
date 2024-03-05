7cf2d6c188c7b50e1dfa382344985e88f7979c0b
Ismael Nascimento <72841488+ismaelnascimento@users.noreply.github.com>
2024-03-05 14:29:15 -0300

    upload QR05

--- CAP-04/Exercicios-Resolvidos/QR05/src/br/edu/principal/Principal.java
+++ CAP-04/Exercicios-Resolvidos/QR05/src/br/edu/principal/Principal.java
@@ -1,33 +1,67 @@
 package br.edu.principal;
 
-import java.util.Scanner;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		double num1,num2,num3,num4;
-		Scanner escaneador = new Scanner(System.in);
+		int dia, mes, ano, hora, min;
+
+		Calendar cal = Calendar.getInstance();
+		Date d = new Date();
+		cal.setTime(d);
+		dia = cal.get(Calendar.DAY_OF_MONTH);
+		mes = cal.get(Calendar.MONTH) + 1;
+		ano = cal.get(Calendar.YEAR);
 		
-		System.out.println("Digite três números em ordem crescente: ");
+		System.out.print("Data Atual: " + dia + "/" + mes + "/" + ano + " - ");
 		
-		num1 = escaneador.nextDouble();
-		num2 = escaneador.nextDouble();
-		num3 = escaneador.nextDouble();
-		
-		System.out.println("Digite um número (fora de ordem):");
-		
-		num4 = escaneador.nextDouble();
-		
-		if (num4 > num3) {
-			System.out.println("A ordem decrescente é: " + num4 + " - " + num3 + " - " + num2 + " - " + num1);
-		} else if (num4 > num2 && num4 < num3) {
-			System.out.println("A ordem decrescente é: " + num3 + " - " + num4 + " - " + num2 + " - " + num1);
-		} else if (num4 > num1 && num4 < num2) {
-			System.out.println("A ordem decrescente é: " + num3 + " - " + num2 + " - " + num4 + " - " + num1);
-		} else if (num4 < num1) {
-			System.out.println("A ordem decrescente é: " + num3 + " - " + num2 + " - " + num1 + " - " + num4);
+		switch (mes) {
+		  case 1: 
+			  System.out.println("janeiro");
+			  break;
+		  case 2: 
+			  System.out.println("fevereiro");
+			  break;
+		  case 3: 
+			  System.out.println("março");
+			  break;
+		  case 4: 
+			  System.out.println("abril");
+			  break;
+		  case 5: 
+			  System.out.println("maio");
+			  break;
+		  case 6: 
+			  System.out.println("junho");
+			  break;
+		  case 7: 
+			  System.out.println("julho");
+			  break;
+		  case 8: 
+			  System.out.println("agosto");
+			  break;
+		  case 9: 
+			  System.out.println("setembro");
+			  break;
+		  case 10: 
+			  System.out.println("outubro");
+			  break;
+		  case 11: 
+			  System.out.println("novembro");
+			  break;
+		  case 12: 
+			  System.out.println("dezembro");
+			  break;
+		  default: System.out.println("month not found :/");
 		}
 		
+		hora = cal.get(Calendar.HOUR_OF_DAY);
+		min = cal.get(Calendar.MINUTE);
+		
+		System.out.print("Hora Atual: ");
+		System.out.println(hora + ":" + min);
 	}
 
 }
