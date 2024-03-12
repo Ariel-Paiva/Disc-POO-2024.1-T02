0c15a5e5719b094552fa3f184959530907664b19
Ismael Nascimento <72841488+ismaelnascimento@users.noreply.github.com>
2024-03-12 17:13:03 -0300

    upload QR24

--- CAP-04/Exercicios-Resolvidos/QR24/src/br/edu/principal/Principal.java
+++ CAP-04/Exercicios-Resolvidos/QR24/src/br/edu/principal/Principal.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		double pre, valor_adic = 0, imposto, pre_custo, desconto, novo_pre;
+		double pre, valor_adic = 0, imposto = 0, pre_custo = 0, desconto = 0, novo_pre = 0;
 		String tipo, refrig;
 		
 		Scanner sc = new Scanner(System.in);
@@ -17,22 +17,22 @@ public class Principal {
 		System.out.println("Digite a refrigeração do produto (S — produto que necessita de refrigeração; e N — produto que não necessita de refrigeração): ");
 		refrig = sc.next();
 		
-		if (refrig == "N") {
-			if (tipo == "A") {
+		if (refrig.equals("N")) {
+			if (tipo.equals("A")) {
 				if (pre < 15) {
 					valor_adic = 2;
 				} else {
 					valor_adic = 5;
 				}
 			} else
-            if (tipo == "L") {
+            if (tipo.equals("L")) {
             	if (pre < 10) {
 					valor_adic = 1.50;
 				} else {
 					valor_adic = 2.50;
 				}
 			}else
-            if (tipo == "V") {
+            if (tipo.equals("V")) {
             	if (pre < 30) {
 					valor_adic = 3;
 				} else {
@@ -40,30 +40,29 @@ public class Principal {
 				}
             }
 		} else {
-            if (tipo == "A") {
+            if (tipo.equals("A")) {
 				valor_adic = 8;
 			}else
-            if (tipo == "L") {
+            if (tipo.equals("L")) {
 				valor_adic = 0;
 			}else
-            if (tipo == "V") {
+            if (tipo.equals("V")) {
 	            valor_adic = 0;
             }
 		}
 		System.out.println("Valor adicional: " + valor_adic);
 		
 		if (pre < 25) {
-			imposto = 5/100 * pre;
+			imposto = (5./100.) * pre;
 		} else {
-			imposto = 8/100 * pre;
+			imposto = (8./100.) * pre;
 		}
 		System.out.println("Imposto: " + imposto);
 		
 		pre_custo = pre + imposto;
 		System.out.println("Preço de custo: " + pre_custo);
-		
-		if (tipo != "A" && refrig != "S") {
-			desconto = 3/100 * pre_custo;
+		if (!tipo.equals("A") && !refrig.equals("S")) {
+			desconto = 3./100. * pre_custo;
 		} else {
 			desconto = 0;
 		}
