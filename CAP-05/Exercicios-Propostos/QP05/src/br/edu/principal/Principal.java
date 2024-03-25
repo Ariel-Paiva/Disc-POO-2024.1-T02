25c2fcb0ce45b4ce6bbaeebcee195f632454015a
João Gabriel Aguiar  de Senna <134979228+Jot4g3@users.noreply.github.com>
2024-03-25 17:27:09 -0300

    QP05

---
+++ CAP-05/Exercicios-Propostos/QP05/src/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		for (int x = 1; x<=10; x++) {
+			for (int y = 1; y<=10; y++) {
+				System.out.println(x+" multiplicado por "+y+" resulta em "+ x*y);
+			}
+			System.out.println("                                                                                        ");
+		}
+
+	}
+
+}
